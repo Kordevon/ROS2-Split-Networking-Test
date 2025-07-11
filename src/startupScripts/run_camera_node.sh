7e4fe5d3fa831dd43bd68dfff97f463f492e2328
Kevin Burston <burstonkevin@gmail.com>
2025-07-11 04:27:26 +0000

    Added DDS profiles and scripts to run nodes

---
+++ src/startupScripts/run_camera_node.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PROFILE=${1:-camera_profile.xml}
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+DDS_PROFILE_PATH="$SCRIPT_DIR/$PROFILE"
+
+docker run --rm --network=host \
+  -e FASTRTPS_DEFAULT_PROFILES_FILE=/root/dds_profile.xml \
+  -v "$DDS_PROFILE_PATH:/root/dds_profile.xml:ro" \
+  ros2-multi-test \
+  bash -c "source /opt/ros/humble/setup.bash && \
+           source /root/talkers/install/setup.bash && \
+           ros2 run camera_node_pkg camera_node"
